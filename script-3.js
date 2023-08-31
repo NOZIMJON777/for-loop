a351f49e0dd69ac6d0d6edb68a51d9f5fab13995
NOZIMJON OYDINOV <nozimjonoydinov@NOZIMJONs-MacBook-Air.local>
2023-08-31 12:55:54 +0900

    script-3

--- script-3.js
+++ script-3.js
@@ -1,6 +1,10 @@
 let year = 2020;
 
-for (i = 2017; i <= 2020; i++) {
+for (i = 2017; i <= 2024; i++) {
     // console.log(i);
-    if ()
+    if (i % 4 === 0) {
+        console.log(i, 'kabisa yili');
+    } else {
+        console.log(i, 'kabisa yili emas');
+    }
 }
